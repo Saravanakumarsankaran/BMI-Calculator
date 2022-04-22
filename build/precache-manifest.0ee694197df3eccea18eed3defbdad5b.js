ecda9898519126dba70f51490b9d1408a00cdb42
sksoftsuave <saravanakumar.sankaran@softsuave.com>
2022-04-22 16:32:21 +0530

    .gitignore changed

---
+++ build/precache-manifest.0ee694197df3eccea18eed3defbdad5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb5e1affca979b5e477ed29262d04b2",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "b4aa88851593ac880bfa",
+    "url": "/bmi-calculator/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "ffaf9e081602bebe604e",
+    "url": "/bmi-calculator/static/css/main.160e4576.chunk.css"
+  },
+  {
+    "revision": "b4aa88851593ac880bfa",
+    "url": "/bmi-calculator/static/js/2.0e5f39ad.chunk.js"
+  },
+  {
+    "revision": "ffaf9e081602bebe604e",
+    "url": "/bmi-calculator/static/js/main.936aa737.chunk.js"
+  },
+  {
+    "revision": "9b25b4e690523b173542",
+    "url": "/bmi-calculator/static/js/runtime~main.425d477f.js"
+  }
+]);
